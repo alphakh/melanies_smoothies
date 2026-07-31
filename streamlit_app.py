7f275310a68dbd884d72c14a0226feb3ab0f99ed
alphakh <konda.harish@alumni.purdue.edu>
2026-07-31 15:56:49 -0400

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,60 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# App title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+     """Choose the fruits you want in your custom Smoothie!
+     """)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+session = get_active_session()
+
+# Editable orders table
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
+editable_df = st.data_editor(my_dataframe)
+
+submitted = st.button('Submit')
+
+if submitted:
+    og_dataset = session.table("smoothies.public.orders")
+    edited_dataset = session.create_dataframe(editable_df)
+
+    merge_result = og_dataset.merge(
+        edited_dataset,
+        (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID']),
+        [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+    )
+
+    st.write(merge_result)
+    st.success("Someone clicked the button.", icon="👉")
+
+# Ingredient selection
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+ingredients_list = st.multiselect(
+    'choose up to 5 ingredients:'
+    , my_dataframe
+    ,max_selections=5
+   )
+
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+                    values ('""" + ingredients_string + """')"""
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
