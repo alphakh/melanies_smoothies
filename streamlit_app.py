840368375f110408158fafe1e104b0c79c0d7aad
alphakh <konda.harish@alumni.purdue.edu>
2026-07-31 19:15:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,12 @@ ingredients_list = st.multiselect(
     ,max_selections=5
    )
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+import requests
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
 st.text(smoothiefroot_response.json())
 
 if ingredients_list:
